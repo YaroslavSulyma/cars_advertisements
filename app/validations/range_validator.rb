9f1d79a80453f88aea427022750f22d59aa03204
YaroslavSulyma <yaryksulima@gmail.com>
2023-02-09 20:47:51 +0200

    added range validator

---
+++ app/validations/range_validator.rb
@@ -0,0 +1,31 @@
+class RangeValidator
+  def self.valid?(range_values)
+    new(range_values).valid?
+  end
+
+  def initialize(range_values)
+    @range_values = range_values
+  end
+
+  def valid?
+    from_or_to_valid_number? && from_less_then_or_eq_to?
+  end
+
+  private
+
+  attr_reader :range_values
+
+  def from_less_then_or_eq_to?
+    from = value_is_valid_number?(range_values[:from]) ? range_values[:from].to_i : -Float::INFINITY
+    to = value_is_valid_number?(range_values[:to]) ? range_values[:to].to_i : Float::INFINITY
+    from <= to ? true : abort('Invalid range values')
+  end
+
+  def from_or_to_valid_number?
+    value_is_valid_number?(range_values[:from]) || value_is_valid_number?(range_values[:to])
+  end
+
+  def value_is_valid_number?(value)
+    value.to_i.to_s == value
+  end
+end
