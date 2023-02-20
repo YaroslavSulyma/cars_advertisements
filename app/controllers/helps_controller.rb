d025362ea2036aeef8187ae2ed575a819d886d5e
YaroslavSulyma <yaryksulima@gmail.com>
2023-02-21 00:17:26 +0200

    added Ukrainian localization

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,20 +1,13 @@
 class HelpsController < ApplicationController
-  # rubocop:disable Metrics/MethodLength, I18n/RailsI18n/DecorateString
   def index
     @menu_options = {
-      'Help' => 'This page provides information about each menu option',
-      'Car List' => 'On the car list page, you can see a list of all the cars in the database.
-You can sort the list by various criteria, like: year, price, odometer, date_added',
-      'Search' => 'The search page allows you to search for specific cars in the database.
-You can filter the search results by make, model, year_from, year_to, price_from and price_to',
-      'My searches' => 'On the my searches page, you can see a list of you search history.
-This feature appears only for logged-in users',
-      'Sign in' => 'Log in to your account to access additional features',
-      'Sign up' => 'On this page you can create a new account to access additional features',
-      'Sign out' => 'This button only appears for logged-in users.
-To log out of your account and end your session, please do so'
+      t('menu.help_option') => t('menu.help_description'),
+      t('menu.cars_list_option') => t('menu.cars_list_description'),
+      t('menu.search_option') => t('menu.search_description'),
+      t('menu.user_searches_option') => t('menu.user_searches_description'),
+      t('menu.sign_in_option') => t('menu.sign_in_description'),
+      t('menu.sign_up_option') => t('menu.sign_up_description'),
+      t('menu.sign_out_btn') => t('menu.sign_out_description')
     }
   end
-
-  # rubocop:enable Metrics/MethodLength, I18n/RailsI18n/DecorateString
 end
--- app/validations/range_validator.rb
+++ app/validations/range_validator.rb
@@ -18,7 +18,7 @@ class RangeValidator
   def from_less_then_or_eq_to?
     from = value_is_valid_number?(range_values[:from]) ? range_values[:from].to_i : -Float::INFINITY
     to = value_is_valid_number?(range_values[:to]) ? range_values[:to].to_i : Float::INFINITY
-    from <= to ? true : abort('Invalid range values')
+    from <= to ? true : abort(t('errors.invalid_range'))
   end
 
   def from_or_to_valid_number?
