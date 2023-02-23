0383dc6a960f6465ba9a0a37ca38c08485b7eb15
YaroslavSulyma <yaryksulima@gmail.com>
2023-02-23 21:12:16 +0200

    fixed bug with sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = column == params[:order_by] && params[:direction] == 'ASC' ? 'DESC' : 'ASC'
-    link_to title, order_by: column, direction:
+    link_to title, request.params.merge(order_by: column, direction:)
   end
 
   def locale_options
