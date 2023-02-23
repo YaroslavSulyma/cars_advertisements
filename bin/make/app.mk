3cc44c8af473d73b7bbe7bcdfe5ac8ed16442da4
YaroslavSulyma <yaryksulima@gmail.com>
2023-02-23 21:11:10 +0200

    added fasterer to make commands [fix-style and fix-style!]

--- bin/make/app.mk
+++ bin/make/app.mk
@@ -13,10 +13,10 @@ style:
 	docker-compose exec app rubocop
 
 fix-style:
-	docker-compose exec app rubocop
+	docker-compose exec app rubocop && docker-compose exec app fasterer
 
 fix-style!:
-	docker-compose exec app rubocop -A
+	docker-compose exec app rubocop -A && docker-compose exec app fasterer
 
 routes:
 	docker-compose exec app rails routes | grep $(RUN_ARGS)
