4332a59813f5eeb2a40635b7c9584ee1ad92e82b
YaroslavSulyma <yaryksulima@gmail.com>
2023-02-26 17:11:01 +0200

    added generators for rspec (generate factory for each model)

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,3 +1,5 @@
 Rails.application.config.generators do |g|
   g.orm :active_record, primary_key_type: :uuid
+  g.test_framework :rspec
+  g.fixture_replacement :factory_bot, dir: 'spec/factories'
 end
