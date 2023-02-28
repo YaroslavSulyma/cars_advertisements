6859141a701dbe503ae55dd930c73d8f0f95ec90
YaroslavSulyma <yaryksulima@gmail.com>
2023-03-01 00:52:50 +0200

    changed date_between to in_date_period for Car factory date_added attribute

--- spec/factories/cars.rb
+++ spec/factories/cars.rb
@@ -6,6 +6,6 @@ FactoryBot.define do
     odometer { Faker::Vehicle.mileage }
     price { Faker::Commerce.price(range: 10_000..100_000).to_i }
     description { Faker::Vehicle.drive_type }
-    date_added { Faker::Date.between(from: '2010-09-23', to: '2023-09-23') }
+    date_added { Faker::Date.in_date_period }
   end
 end
