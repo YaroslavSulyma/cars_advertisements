9e135c5a64b38f8a7fd74999285a47029e0e304e
YaroslavSulyma <yaryksulima@gmail.com>
2023-03-02 21:11:07 +0200

    added shared exmample for pagination, moved prams into let

--- spec/queries/search_query_spec.rb
+++ spec/queries/search_query_spec.rb
@@ -1,7 +1,24 @@
 RSpec.describe SearchQuery do
   describe '#call' do
+    subject(:query) { described_class.new }
+    subject(:result) { query.call(params).records }
+
+    let(:search1) { create(:search, updated_at: 1.day.ago) }
+    let(:search2) { create(:search, updated_at: 2.days.ago) }
+
+    context 'ordering' do
+      let(:params) { {} }
+
+      it 'orders by the default search attribute if no order param is passed' do
+        expect(result).to match_array([search1, search2].sort_by(&:updated_at))
+      end
+    end
+
     context 'with pagination params' do
-      it_behaves_like 'paginatable', 'Search'
+      it_behaves_like 'paginatable',
+                      'Search',
+                      described_class::DEFAULT_ORDER_ATTRIBUTE,
+                      described_class::DEFAULT_DIRECTION
     end
   end
 end
